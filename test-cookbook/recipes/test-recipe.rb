9af64afcf2537c55bdb69a7a4688f77c44357f42
khaiyum <khaiyum55@gmail.com>
2022-02-02 14:30:18 +0000

    added Httpd in recipe

--- test-cookbook/recipes/test-recipe.rb
+++ test-cookbook/recipes/test-recipe.rb
@@ -3,3 +3,17 @@
 # Recipe:: test-recipe
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+package 'httpd' do
+action :install
+end
+
+file '/var/www/html/index.html' do
+content 'Welcome to Technical Guftgu'
+action :create
+end
+
+
+service 'httpd' do
+action [:enable, :start]
+end
+
